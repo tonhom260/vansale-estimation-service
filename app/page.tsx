b8f8c2cdfc693fa683fab92d8fb128fa5cacf372
tonhom260 <tonhom260@gmail.com>
2025-12-26 22:41:16 +0700

    create table with enter next node

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 import getProductList from "@/action/product/get";
 import getUserList, { TUser } from "@/action/user/get";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
 
@@ -30,80 +29,88 @@ export default function Home() {
   }, [])
 
   const [user, setUser] = useState<TUser[]>([])
-  console.log(user)
+  // console.log(user)
 
   async function getUsers() {
     const user = await getUserList()
     setUser(user!)
   }
+
   async function getProduct() {
     console.log("object")
     const product = await getProductList()
     console.log(product)
   }
+
   useEffect(() => {
     getProduct()
     getUsers()
   }, [])
 
+  // impl กด enter แล้ว ขยับ
+  const input1Ref = useRef<HTMLInputElement>(null)
+  const input2Ref = useRef<HTMLInputElement>(null)
+  const input3Ref = useRef<HTMLInputElement>(null)
+  const refList = [input1Ref, input2Ref, input3Ref]
+
+  const handleKeyDown = (e: React.KeyboardEvent, nextRef: React.RefObject<HTMLInputElement | null>) => {
+    if (e.key === "Enter") {
+      e.preventDefault() // ป้องกันการเผลอ submit form
+      nextRef.current?.focus() // ย้าย focus ไปที่ถัดไป
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+      <main className="flex min-h-screen w-full  flex-col items-center justify-between  bg-white  sm:items-start p-8">
+        <table className="table-auto w-full min-w-[1400px]  border-separate border-spacing-0 border rounded-xl  text-black text-sm bg-white">
+          <thead className=''>
+            <tr className='border-b pl-11  h-10 '>
+              <th className='flex-1 border-b pl-8 text-start text-[14px] min-w-[220px]  font-[400]'>วันที่</th>
+              <th className='border-b pl-2 text-start text-[14px] w-full   font-[400]'>รายละเอียด</th>
+              <th className='border-b pl-2 text-start text-[14px] min-w-[120px]   font-[400]'>การจัดการ</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className='min-h-14 h-14  text-sm  text-[18px]'>
+              <td className={` ${true ? "" : "border-b"} pl-8 h-14   flex items-center  `}>
+                <input
+                  ref={refList?.at(0)}
+                  onKeyDown={(e) => handleKeyDown(e, refList!.at(1)!)}
+                />
+              </td>
+              <td className={` ${true ? "" : "border-b"} pl-2 text-[#0A0A0A] `} >{"desc"}</td>
+              <td className={` ${true ? "" : "border-b"} pl-2 text-[#0A0A0A] `} >
+                <div className='flex text-[#000000] '>
+                  <div className='w-4' />
+                </div>
+              </td>
+            </tr>
+            <tr className='min-h-14 h-14  text-sm  text-[18px]'>
+              <td className={` ${true ? "" : "border-b"} pl-8 h-14   flex items-center  `}>
+                <input ref={refList?.at(1)} onKeyDown={(e) => handleKeyDown(e, refList!.at(2)!)} />
+              </td>
+              <td className={` ${true ? "" : "border-b"} pl-2 text-[#0A0A0A] `} >{"desc"}</td>
+              <td className={` ${true ? "" : "border-b"} pl-2 text-[#0A0A0A] `} >
+                <div className='flex text-[#000000] '>
+                  <div className='w-4' />
+                </div>
+              </td>
+
+            </tr>
+            <tr className='min-h-14 h-14  text-sm  text-[18px]'>
+              <td className={` ${true ? "" : "border-b"} pl-8 h-14   flex items-center  `}>
+                <input ref={refList?.at(2)} />
+              </td>
+              <td className={` ${true ? "" : "border-b"} pl-2 text-[#0A0A0A] `} >{"desc"}</td>
+              <td className={` ${true ? "" : "border-b"} pl-2 text-[#0A0A0A] `} >
+                <div className='flex text-[#000000] '>
+                  <div className='w-4' />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </main>
     </div>
   );
